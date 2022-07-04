d34e95f9fe7c96df3637a4f8a08225f770e458ee
shirokovnv <shirokovnv@gmail.com>
2022-07-04 20:41:09 +0500

    Remove unneccessary @doc

--- lib/phx_json_rpc_web/controllers/test_controller.ex
+++ lib/phx_json_rpc_web/controllers/test_controller.ex
@@ -3,12 +3,10 @@ defmodule PhxJsonRpcWeb.TestController do
 
   alias PhxJsonRpc.Error.ServerError
 
-  @doc false
   def hello(%{"name" => name}) do
     "Hello, #{name}"
   end
 
-  @doc false
   def internal_error(_params) do
     raise "oops"
   end
@@ -17,7 +15,6 @@ defmodule PhxJsonRpcWeb.TestController do
     raise ServerError
   end
 
-  @doc false
   def without_schema_ref(_params) do
     "no validation provided"
   end
