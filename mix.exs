c0fd4878264b9ab130d3bbf9097216082d3a8081
shirokovnv <shirokovnv@gmail.com>
2022-07-19 21:08:47 +0500

    Add core path to dialyzer config

--- mix.exs
+++ mix.exs
@@ -54,6 +54,7 @@ defmodule PhxJsonRpc.MixProject do
 
       # Dialyzer
       dialyzer: [
+        plt_core_path: "priv/plts",
         plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
         plt_add_deps: :apps_direct,
         plt_add_apps: [:ex_json_schema, :jason]
