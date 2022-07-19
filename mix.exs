493b7931a667ca413f18d1a50b5ef13b41df9ab5
shirokovnv <shirokovnv@gmail.com>
2022-07-19 21:15:34 +0500

    Drop plt_file option

--- mix.exs
+++ mix.exs
@@ -55,7 +55,6 @@ defmodule PhxJsonRpc.MixProject do
       # Dialyzer
       dialyzer: [
         plt_core_path: "priv/plts",
-        plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
         plt_add_deps: :apps_direct,
         plt_add_apps: [:ex_json_schema, :jason]
       ]
